69639b5927f297976fb28992107433dd8fad8aa4
unknown <yannick@schaufel.net>
2020-01-07 14:41:15 +0100

    modifs mineures

--- YS_fonctions.cpp
+++ YS_fonctions.cpp
@@ -10,49 +10,43 @@ Plane layerCube(Cube scanner, Dimension dimension, int layer) {
       case Dimension::XY:
 
          output.resize(scanner.size());
-
          for (size_t i = 0; i < scanner.size(); ++i) {
-
             output.at(i).resize(scanner.at(i).size());
-
-            for (size_t j = 0; j < scanner.at(i).size(); ++j) {
+            for (size_t j = 0; j < scanner.at(i).size(); ++j)
                output[i][j] = scanner[i][j][layer];
-            }
          }
 
          break;
+
       case Dimension::XZ:
 
          output.resize(scanner.size());
-
          for (size_t i = 0; i < scanner.size(); ++i) {
-
             output.at(i).resize(scanner.at(i).at(layer).size());
-
-            for (size_t j = 0; j < scanner.at(i).at(layer).size(); ++j) {
+            for (size_t j = 0; j < scanner.at(i).at(layer).size(); ++j)
                output[i][j] = scanner[i][layer][j];
-            }
          }
-
-         break;
-      case Dimension::YZ:
-
-         output.resize(scanner.at(layer).size());
-
-         for (size_t i = 0; i < scanner.size(); ++i) {
-
-            output.at(i).resize(scanner.at(layer).at(i).size());
-
-            for (size_t j = 0; j < scanner.at(layer).at(i).size(); ++j) {
-               output[i][j] = scanner[layer][i][j];
-            }
-         }
-
-         break;
-      default:
-         break;
-
    }
+
+   break;
+
+   case Dimension::YZ:
+
+   output.resize(scanner.at(layer).size());
+   for (size_t i = 0; i < scanner.size(); ++i) {
+      output.at(i).resize(scanner.at(layer).at(i).size());
+      for (size_t j = 0; j < scanner.at(layer).at(i).size(); ++j) {
+         output[i][j] = scanner[layer][i][j];
+      }
+   }
+
+   break;
+
+   default:
+
+   //On fait quoi en default ? On ignore le cas ?
+
+   break;
 
    return output;
 
